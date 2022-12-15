5f713a7f7d2039f2dc5fb370bd7eb78c8a8a5a57
HarveyNilton <65696942+HarveyNilton@users.noreply.github.com>
2022-12-15 18:49:02 -0500

    mejora mtk 1

--- src/component/QuoteBox.jsx
+++ src/component/QuoteBox.jsx
@@ -5,7 +5,7 @@ import quotes from '/src/data/quotes.json'
 const QuoteBox = () => {
 
     const [index, setIndex]=useState(0)
-    const [color, setColor] = useState('#7B1FA2')
+    const [color, setColor] = useState('7B1FA2')
 
     let randomIndex = Math.floor(Math.random()*quotes.length)
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -22,7 +22,13 @@ const QuoteBox = () => {
         <div className="container-general" style={{background:`#${color}`}}>
             <div className="container-tarjet">
                 <section className="container-quote">
-                    <figure className='logo' style={{color:`#${color}`}}>🙶</figure>
+                    <figure className='logo' style={{color:`#${color}`}}>{
+                        <svg style={{fill:`#${color}`}} width="60"        
+                            height="45" viewBox="0 0 40 43" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.9919 7.66663H33.0082C34.7968 7.66663 36.2602 9.22507 36.2602 11.1298V31.909C36.2602 33.8138 34.7968 35.3722 33.0082 35.3722H6.9919C5.20328 35.3722 3.73987 33.8138 3.73987 31.909V11.1298C3.73987 9.22507 5.20328 7.66663 6.9919 7.66663Z" stroke="#ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                            <path d="M36.2602 11.1299L20 23.2511L3.73987 11.1299" stroke="#ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    }</figure>
                     <p className="quote" style={{color:`#${color}`}}>{quotes[index].quote}</p>
                 </section>
                 <section className='container-author'>
